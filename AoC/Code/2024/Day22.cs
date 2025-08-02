0f012daa5bdcef18d7b373e0f6348c5abd4a96f6
mixco1a0 <mixco1a0@gmail.com>
2025-08-02 15:59:35 -0400

    d22 p1

--- AoC/Code/2024/Day22.cs
+++ AoC/Code/2024/Day22.cs
@@ -25,9 +25,20 @@ namespace AoC._2024
                 new Core.TestDatum
                 {
                     TestPart = Core.Part.One,
-                    Output = "",
+                    Output = "5908254",
+                    Variables = new Dictionary<string, string> { { nameof(_SecretNumberCount), "10" } },
                     RawInput =
-@""
+@"123"
+                },
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.One,
+                    Output = "37327623",
+                    RawInput =
+@"1
+10
+100
+2024"
                 },
                 new Core.TestDatum
                 {
@@ -40,9 +51,44 @@ namespace AoC._2024
             return testData;
         }
 
+#pragma warning disable IDE1006 // Naming Styles
+        private static int _SecretNumberCount { get; }
+#pragma warning restore IDE1006 // Naming Styles
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool _)
         {
-            return string.Empty;
+            GetVariable(nameof(_SecretNumberCount), 2000, variables, out int secretNumberCount);
+            List<long> secretNumbers = [.. inputs.Select(long.Parse)];
+            List<long> nextSecrets = [];
+            for (int i = 0; i < secretNumberCount; ++i)
+            {
+                foreach (long secretNumber in secretNumbers)
+                {
+                    // step 1
+                    long working = secretNumber * 64;
+                    working ^= secretNumber;
+                    working %= 16777216;
+                    long newSecretNumber = working;
+
+                    // step 2
+                    working /= 32;
+                    working ^= newSecretNumber;
+                    working %= 16777216;
+                    newSecretNumber = working;
+
+                    // step 3
+                    working *= 2048;
+                    working ^= newSecretNumber;
+                    working %= 16777216;
+
+                    nextSecrets.Add(working);
+
+                    //Log($"{secretNumber} -> {working}");
+                }
+                secretNumbers = [.. nextSecrets];
+                nextSecrets.Clear();
+            }
+            return secretNumbers.Sum().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
