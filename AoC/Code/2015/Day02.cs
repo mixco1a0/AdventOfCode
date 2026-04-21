d325aa881f3bc6a8f0e06c488291939d81033c6f
Carlo Mixco <mixco1a0@gmail.com>
2026-04-20 22:56:42 -0400

    minor cleanup 2015

--- AoC/Code/2015/Day02.cs
+++ AoC/Code/2015/Day02.cs
@@ -10,53 +10,52 @@ namespace AoC._2015
 
         public override string GetSolutionVersion(Core.Part part)
         {
-            switch (part)
+            return part switch
             {
-                case Core.Part.One:
-                    return "v1";
-                case Core.Part.Two:
-                    return "v1";
-                default:
-                    return base.GetSolutionVersion(part);
-            }
+                Core.Part.One => "v1",
+                Core.Part.Two => "v1",
+                _ => base.GetSolutionVersion(part),
+            };
         }
 
         protected override List<Core.TestDatum> GetTestData()
         {
-            List<Core.TestDatum> testData = new List<Core.TestDatum>();
-            testData.Add(new Core.TestDatum
-            {
-                TestPart = Core.Part.One,
-                Output = "58",
-                RawInput =
+            List<Core.TestDatum> testData =
+            [
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.One,
+                    Output = "58",
+                    RawInput =
 @"2x3x4"
-            });
-            testData.Add(new Core.TestDatum
-            {
-                TestPart = Core.Part.One,
-                Output = "101",
-                RawInput =
+                },
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.One,
+                    Output = "101",
+                    RawInput =
 @"2x3x4
 1x1x10"
-            });
-            testData.Add(new Core.TestDatum
-            {
-                TestPart = Core.Part.Two,
-                Output = "34",
-                RawInput =
+                },
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.Two,
+                    Output = "34",
+                    RawInput =
 @"2x3x4"
-            });
-            testData.Add(new Core.TestDatum
-            {
-                TestPart = Core.Part.Two,
-                Output = "14",
-                RawInput =
+                },
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.Two,
+                    Output = "14",
+                    RawInput =
 @"1x1x10"
-            });
+                },
+            ];
             return testData;
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, Func<int, int, int, int> presentsFunction)
+        private static string SharedSolution(List<string> inputs, Dictionary<string, string> variables, Func<int, int, int, int> presentsFunction)
         {
             int needed = 0;
             foreach (string input in inputs)
@@ -66,7 +65,7 @@ namespace AoC._2015
                     continue;
                 }
                 
-                List<int> dims = input.Split("x").Select(c => int.Parse(c)).ToList();
+                List<int> dims = [.. input.Split("x").Select(c => int.Parse(c))];
                 dims.Sort();
                 int x = dims[0], y = dims[1], z = dims[2];
                 needed += presentsFunction(x, y, z);
@@ -76,9 +75,9 @@ namespace AoC._2015
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables, (int x, int y, int z) => 2 * x * y + 2 * x * z + 2 * y * z + x * y);
+            => SharedSolution(inputs, variables, (x, y, z) => 2 * x * y + 2 * x * z + 2 * y * z + x * y);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables, (int x, int y, int z) => 2 * x + 2 * y + x * y * z);
+            => SharedSolution(inputs, variables, (x, y, z) => 2 * x + 2 * y + x * y * z);
     }
 }
