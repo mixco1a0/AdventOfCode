4644cb6fec70fc2bb9f496a5acf4128d4e9b91a9
mixco1a0 <mixco1a0@gmail.com>
2026-08-27 16:21:32 -0400

    moving the entry point

---
+++ AoC/Solid/Engine/Program.cs
@@ -0,0 +1,13 @@
+using AoC.Solid.Core.Interfaces;
+using AoC.Solid.Core;
+using AoC.Solid.Solutions.Y2015;
+
+namespace AoC.Solid.Engine;
+
+internal class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
