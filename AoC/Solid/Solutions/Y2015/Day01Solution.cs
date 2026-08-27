4533ee1d5edc90b203de1d7e953116a081d0302c
mixco1a0 <mixco1a0@gmail.com>
2026-08-27 16:26:01 -0400

    adding initial day for testing

---
+++ AoC/Solid/Solutions/Y2015/Day01Solution.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using AoC.Solid.Core.Interfaces;
+
+namespace AoC.Solid.Solutions.Y2015;
+
+public class Day01Solution : IDaySolution
+{
+    public int Day => 1;
+    public int Year => 2015;
+
+    public string SolvePart1(IInputProvider inputProvider)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string SolvePart2(IInputProvider inputProvider)
+    {
+        throw new System.NotImplementedException();
+    }
+}
