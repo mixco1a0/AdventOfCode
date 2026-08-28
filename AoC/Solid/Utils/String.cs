9d412363bed9812525a8cd4604a71fa9bd2850af
mixco1a0 <mixco1a0@gmail.com>
2026-08-28 14:09:34 -0400

    adding string utils

---
+++ AoC/Solid/Utils/String.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.Solid.Utils;
+
+
+public static class String
+{    
+    public static IEnumerable<string> SplitAndTrim(string input, char seperator, char trim)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return [];
+        }
+
+        return input.Split(seperator).Select(str => str.Trim(trim));
+    }
+
+    public static string[] Split(string input, char seperator)
+    {
+        return input.Split(seperator, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public static string[] Split(string input, string seperator)
+    {
+        return input.Split(seperator.ToCharArray(), StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    }
+}
