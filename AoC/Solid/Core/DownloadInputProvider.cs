c5d30aeae9bb9f90e251446862144b995e65f97b
mixco1a0 <mixco1a0@gmail.com>
2026-08-28 14:50:00 -0400

    adding download input provider

---
+++ AoC/Solid/Core/DownloadInputProvider.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AoC.Solid.Core.Interfaces;
+using AoC.Solid.Utils;
+
+namespace AoC.Solid.Core;
+
+public class DownloadInputProvider(int year, int day) : IInputProvider
+{
+    private int _year = year;
+    private int _day = day;
+    private string _input = string.Empty;
+
+
+    public IEnumerable<string> GetInput()
+    {
+        return Puzzle.ConvertInput(_input);
+    }
+
+    public void DownloadInput()
+    {
+        if (string.IsNullOrEmpty(_input))
+        {
+            Task task = Task.Run(async () => await DownloadInputAsync());
+            task.Wait();
+        }
+    }
+
+    private async Task DownloadInputAsync()
+    {
+        const string sessionEnvironmentVariable = "AOC_SESSION";
+        string? session = Environment.GetEnvironmentVariable(sessionEnvironmentVariable);
+        if (string.IsNullOrEmpty(session))
+        {
+            throw new Exception($"Environment variable \"{sessionEnvironmentVariable}\" was not found.");
+        }
+
+        const string baseAddress = "https://adventofcode.com";
+        Uri uri = new(baseAddress);
+        CookieContainer cookieContainer = new();
+        cookieContainer.Add(uri, new Cookie("session", session));
+
+        using HttpClientHandler httpClientHandler = new() { CookieContainer = cookieContainer };
+        using HttpClient httpClient = new(httpClientHandler) { BaseAddress = uri };
+
+        string url = $"{_year}/day/{_day}/input";
+        HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+        httpResponseMessage.EnsureSuccessStatusCode();
+        _input = await httpResponseMessage.Content.ReadAsStringAsync();
+    }
+}
