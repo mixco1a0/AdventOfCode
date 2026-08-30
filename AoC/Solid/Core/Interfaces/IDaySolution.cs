ef79f78d05c6dc57e58ee94cb1e10a38296b85e2
mixco1a0 <mixco1a0@gmail.com>
2026-08-30 00:23:59 -0400

    minor cleanup

--- AoC/Solid/Core/Interfaces/IDaySolution.cs
+++ AoC/Solid/Core/Interfaces/IDaySolution.cs
@@ -2,9 +2,9 @@ namespace AoC.Solid.Core.Interfaces;
 
 public interface IDaySolution
 {
-    public int Day { get; }
-    public int Year { get; }
+    int Day { get; }
+    int Year { get; }
 
-    public string SolvePart1(IInputProvider inputProvider);
-    public string SolvePart2(IInputProvider inputProvider);
+    string SolvePart1(IInputProvider inputProvider);
+    string SolvePart2(IInputProvider inputProvider);
 }
