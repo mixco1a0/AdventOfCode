fc49e6b9ba03a58dee14f172ed6b5d5a009531b9
mixco1a0 <mixco1a0@gmail.com>
2026-08-31 15:00:38 -0400

    adding additional days for testing

---
+++ AoC/Solid/Solutions/Y2015/Day02Solution.cs
@@ -0,0 +1,19 @@
+using AoC.Solid.Core.Interfaces;
+
+namespace AoC.Solid.Solutions.Y2015;
+
+public class Day02Solution : IDaySolution
+{
+    public int Day => 2;
+    public int Year => 2015;
+
+    public string SolvePart1(IInputProvider inputProvider)
+    {
+        return string.Empty;
+    }
+
+    public string SolvePart2(IInputProvider inputProvider)
+    {
+        return string.Empty;
+    }
+}
---
+++ AoC/Solid/Solutions/Y2016/Day01Solution.cs
@@ -0,0 +1,19 @@
+using AoC.Solid.Core.Interfaces;
+
+namespace AoC.Solid.Solutions.Y2016;
+
+public class Day01Solution : IDaySolution
+{
+    public int Day => 1;
+    public int Year => 2016;
+
+    public string SolvePart1(IInputProvider inputProvider)
+    {
+        return string.Empty;
+    }
+
+    public string SolvePart2(IInputProvider inputProvider)
+    {
+        return string.Empty;
+    }
+}
