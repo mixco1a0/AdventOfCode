ec545748adcf0e7539ab440b202ef3b743028393
mixco1a0 <mixco1a0@gmail.com>
2026-09-02 16:51:25 -0400

    adding math utils

---
+++ AoC/Solid/Utils/Math/Number.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.Solid.Utils.Math;
+
+public static class Number
+{
+#region int functions
+    /// <summary>
+    /// Return the positive modulo using provided values
+    /// </summary>
+    /// <param name="dividend">value to be modded</param>
+    /// <param name="divisor">the number value needs to be modded by</param>
+    /// <returns></returns>
+    public static int Mod(int dividend, int divisor)
+    {
+        return (dividend % divisor + divisor) % divisor;
+    }
+
+    /// <summary>
+    /// Call split on a string and then parse it into ints
+    /// </summary>
+    /// <param name="input">string of separated ints</param>
+    /// <param name="seperators">characters used as seperators</param>
+    /// <returns></returns>
+    public static IEnumerable<int> Split(string input, string seperators)
+    {
+        string[] split = String.Split(input, seperators);
+        return split.Where(s => int.TryParse(s, out int result)).Select(int.Parse);
+    }
+
+    /// <summary>
+    /// Call split on a string and then parse it into ints
+    /// </summary>
+    /// <param name="input">string of separated ints</param>
+    /// <param name="seperator">character used as seperator</param>
+    /// <returns></returns>
+    public static IEnumerable<int> Split(string input, char seperator)
+    {
+        string[] split = String.Split(input, seperator);
+        return split.Where(s => int.TryParse(s, out int result)).Select(int.Parse);
+    }
+#endregion
+}
---
+++ AoC/Solid/Utils/Math/Vec.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AoC.Solid.Core.Interfaces;
+
+namespace AoC.Solid.Utils.Math;
+
+public record Vec3(int X, int Y, int Z);
+
+internal class Vec3InputProvider(IStringInputProvider stringInputProvider, Func<string, Vec3> parseFunc) : IInputProvider<Vec3>
+{
+    private readonly IStringInputProvider _stringInputProvider = stringInputProvider;
+    private readonly Func<string, Vec3> _parseFunc = parseFunc ?? DefaultParser;
+    private IEnumerable<Vec3> _vec3 = [];
+
+    public IEnumerable<Vec3> GetInput()
+    {
+        if (!_vec3.Any())
+        {
+            _vec3 = _stringInputProvider.GetInput().Select(s => _parseFunc(s));
+        }
+        return _vec3;
+    }
+
+    private static Vec3 DefaultParser(string input)
+    {
+        IEnumerable<int> ints = Number.Split(input, ',');
+        return new(ints.First(), ints.Skip(1).First(), ints.Skip(2).First());
+    }
+}
