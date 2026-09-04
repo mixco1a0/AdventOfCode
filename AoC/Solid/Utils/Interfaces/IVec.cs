5f9aecf45e0f86c6da7d3f58083f07806b5a1ea9
mixco1a0 <mixco1a0@gmail.com>
2026-09-04 12:50:48 -0400

    adding IVec

---
+++ AoC/Solid/Utils/Interfaces/IVec.cs
@@ -0,0 +1,17 @@
+using System.Numerics;
+
+namespace AoC.Solid.Utils.Interfaces;
+
+public interface IVec<VecT, T> :
+    IAdditionOperators<VecT, VecT, VecT>,
+    ISubtractionOperators<VecT, VecT, VecT>,
+    IMultiplyOperators<VecT, T, VecT>,
+    IDivisionOperators<VecT, T, VecT>,
+    IModulusOperators<VecT, T, VecT>
+    where VecT : IVec<VecT, T>
+{
+    static abstract VecT Zero {get;}
+    static abstract VecT One {get;}
+    
+    public abstract T Manhattan(VecT other);
+}
